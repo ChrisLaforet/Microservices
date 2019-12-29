c6b06676c3e062f905800127fa8a25af2bfec06f
Chris Laforet <Chris.Laforet@shuttereleasefilms.com>
2019-12-29 14:36:50 -0500

    Continuation of reworking queue handling onto a thread

--- SagaBroker/Saga/SagaRemoteDriver.cs
+++ SagaBroker/Saga/SagaRemoteDriver.cs
@@ -1,17 +1,69 @@
-﻿using SagaProxy.QueueManagement;
+﻿using SagaProxy.Message;
+using SagaProxy.QueueManagement;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace SagaBroker.Saga
 {
 	internal class SagaRemoteDriver
 	{
 		private readonly IQueueDriver queueDriver;
+		private readonly IDictionary<string, RequestResponse> pending = new Dictionary<string, RequestResponse>();
+
+		private readonly Thread monitorDriver;
 
 		internal SagaRemoteDriver(IQueueDriver driver)
 		{
 			queueDriver = driver;
+			monitorDriver = new Thread(this.ResponseMonitor);
+			monitorDriver.IsBackground = true;
+			monitorDriver.Start();
+		}
+
+		private void ResponseMonitor()
+		{
+
+		}
+
+		public void SendMessage(IQueueMessage message)
+		{
+			string correlationID = queueDriver.SendMessage(message);
+			lock (pending)
+			{
+				pending.Add(correlationID, new RequestResponse(message));
+			}
+
+		}
+
+	}
+
+	class RequestResponse
+	{
+		public RequestResponse(IQueueMessage request)
+		{
+			Request = request;
+		}
+
+		public readonly DateTime Timestamp = DateTime.Now;
+		public IQueueMessage Request { private set; get; }
+		public IQueueMessage Response { set; get; }
+
+		public string CorrelationID
+		{
+			get
+			{
+				return Request.CorrelationID;
+			}
+		}
+
+		public bool HasResponse
+		{
+			get
+			{
+				return Response != null;
+			}
 		}
 	}
 }
